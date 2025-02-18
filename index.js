ab442050138977d3390fe5601cb4945a3b71340a
加卷 <103292046+BananaCakeCN@users.noreply.github.com>
2025-02-18 16:38:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ document.getElementById('pds').onclick = function(){
 document.getElementById('ctt').onclick = function(){
     window.scrollTo(0, document.getElementById('cntc').offsetTop - 140)
 }
+document.getElementById('opt').onclick = function(){
+    window.scrollTo(0, document.getElementById('optt').offsetTop - 140)
+}
 var map = L.map('map', {attributionControl: false}).setView([26.078173, 119.305408], 18);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 L.marker([26.078173, 119.305408]).addTo(map)
